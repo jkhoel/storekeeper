68686fbeb0751ae4390f7fe889b28bb32efb49b7
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-13 15:19:12 +0100

    chg: new test for GET /

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,5 +1,18 @@
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+
+import app from '../src/server'
+
+// Configure Chai
+chai.use(chaiHttp)
+chai.should()
+
 describe('The Server:', () => {
-  it('Should start running on port 3000', () => {
-    return true
+  it('Should respond to a basic GET / request', (done) => {
+    chai.request(app).get('/').end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      done();
+    })
   })
 })
