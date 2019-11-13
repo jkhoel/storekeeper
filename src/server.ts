57949f262530db600acb8885a2aa5572bd964a10
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-13 12:31:02 +0100

    pre-webpack commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// lib/app.ts
+import express from 'express'
+
+// Create a new express application instance
+const app: express.Application = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
