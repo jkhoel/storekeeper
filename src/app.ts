04e88d1c3248387d7d5e30f1f4f965deaabb0935
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-13 20:12:43 +0100

    chg: using dotenv instead of key files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 // lib/app.ts
 import express from 'express';
-import keys from '../config/keys'
+
+require('dotenv').config()
+
+const PORT = process.env.PORT           // keys.apiPort
+const ROOT_URL = process.env.ROOT_URL   // keys.rootUrl
 
 // Import API Endpoints
 import endpoint from './routes/default'
@@ -9,10 +13,10 @@ import endpoint from './routes/default'
 const app: express.Application = express();
 
 // Map endpoints to paths
-app.use(keys.rootUrl, endpoint);
+app.use(ROOT_URL, endpoint);
 
 // Start app
-app.listen(keys.apiPort, () => console.log(`Storekeeper listening on port ${keys.apiPort} - NODE_ENV: ${process.env.NODE_ENV}`));
+app.listen(PORT, () => console.log(`Storekeeper listening on port ${PORT} - NODE_ENV: ${process.env.NODE_ENV}`));
 
 // Export our app for testing purposes
 export default app;
