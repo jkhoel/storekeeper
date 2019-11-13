0d8d30a1fd8bbc74935a80f01efe8aefc6128f62
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-13 20:44:02 +0100

    add: airports route

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,14 @@ const ROOT_URL = process.env.ROOT_URL   // keys.rootUrl
 
 // Import API Endpoints
 import endpoint from './routes/default'
+import airports from './routes/airports'
 
 // Create a new express application instance
 const app: express.Application = express();
 
 // Map endpoints to paths
 app.use(ROOT_URL, endpoint);
+app.use(`${ROOT_URL}/airports`, airports);
 
 // Start app
 app.listen(PORT, () => console.log(`Storekeeper listening on port ${PORT} - NODE_ENV: ${process.env.NODE_ENV}`));
---
+++ src/routes/airports.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import sql from '../utils/db';
+
+const airports = express.Router();
+
+airports.get('/', (req, res) => {
+  // Default value if limit is not passed
+  // let limit = 'LIMIT 0, 50';
+  let limit = '';
+  if (req.query.limit) limit = `LIMIT 0, ${req.query.limit}`;
+
+  let desc = '';
+  if (req.query.desc !== 'false') desc = `ORDER BY af_icao DESC`;
+
+  const query = `SELECT * from list_airfields ${desc} ${limit}`;
+  sql(query).then((data) => {
+    if (data.error) res.json(data);
+    res.json(data.rows);
+  });
+});
+
+export default airports;
