3810b030eeb9ff63adeb69d173f84719b69bad0b
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-13 18:11:16 +0100

    chg: updated tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,15 +1,15 @@
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 
-import app from '../src/server'
+import server from '../src/app'
 
 // Configure Chai
 chai.use(chaiHttp)
 chai.should()
 
-describe('The Server:', () => {
-  it('Should respond to a basic GET / request', (done) => {
-    chai.request(app).get('/').end((err, res) => {
+describe('Storekeeper:', () => {
+  it('Should respond to a basic GET /api/v1/ request to root endpoint', (done) => {
+    chai.request(server).get('/api/v1/').end((err, res) => {
       res.should.have.status(200);
       res.body.should.be.a('object');
       done();
