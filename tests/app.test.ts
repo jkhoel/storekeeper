874eda8bca4f2f7b8cc688a498eddf8d793feb45
Jens Kristian Hoel <github@hoelweb.com>
2019-11-15 00:22:51 +0100

    chg: added new test for /api/v1/airports

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -3,6 +3,8 @@ import chaiHttp from 'chai-http';
 
 import app from '../src/app';
 
+import handlerGetAll from '../src/routes/request-handlers/getAllAirports'
+
 // Configure Chai
 chai.use(chaiHttp);
 chai.should();
@@ -36,20 +38,50 @@ describe('Endpoint: /api/v1/:', function () {
 
 
 describe('Endpoint: /api/v1/airports:', () => {
-  it('GET /api/v1/airports - Should return status 200 and all airports as an object', function (done) {
-    chai.request(app).get('/api/v1/api/v1/airports').end(function (err, res) {
-      res.should.have.status(200);
-      res.body.should.be.a('object');
-      done();
-    });
-  });
 
-  it('GET /api/v1/airports?limit=3 - Should return status 200 and an object with 3 airports', function (done) {
-    chai.request(app).get('/api/v1/api/v1/airports').end(function (err, res) {
-      res.should.have.status(200);
-      res.body.should.be.a('object');
-      console.log('res.body: %s', Object.keys(res.body).length)
-      done();
-    });
-  });
+  describe('Handler: getAllAirports', function () {
+    it('returns a list of airfields when Airports.getAll is successful', function (done) {
+      // Mock a request
+      const req = {}
+
+      // Mock some returned data
+      const airportList = [{ airfield_id: 40 }, { airfield_id: 39 }, { airfield_id: 38 }]
+
+      // Create a dummy object that adheres to data-layer/Airport's interface
+      const Airports = {
+        getAll: function (callback: any) { callback(null, airportList) }
+      }
+
+      // Create the handler, pass the dummy Airports object
+      const handleRequest = handlerGetAll(Airports)
+
+      // Call the handler!
+      handleRequest(req, function (err: any, result: any) {
+        if (err) return done(err);
+
+        // Do assertions \o/
+        result.should.have.status(200);
+        // TODO: Do assertion on the type of data returned
+        done()
+      })
+
+    })
+  })
+
+  // it('GET /api/v1/airports - Should return status 200 and all airports as an object', function (done) {
+  //   chai.request(app).get('/api/v1/api/v1/airports').end(function (err, res) {
+  //     res.should.have.status(200);
+  //     res.body.should.be.a('object');
+  //     done();
+  //   });
+  // });
+
+  // it('GET /api/v1/airports?limit=3 - Should return status 200 and an object with 3 airports', function (done) {
+  //   chai.request(app).get('/api/v1/api/v1/airports?limit=3').end(function (err, res) {
+  //     res.should.have.status(200);
+  //     res.body.should.be.a('object');
+  //     console.log('res.body: %s', Object.keys(res.body).length)
+  //     done();
+  //   });
+  // });
 })
