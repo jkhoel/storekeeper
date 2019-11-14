6cc172931d7c13dffb8c69bfd3e8c57931b390f7
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-14 11:25:01 +0100

    chg: added more tests for endpoints

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -15,4 +15,23 @@ describe('Storekeeper:', () => {
       done();
     });
   });
+
+  describe('Endpoint: /api/v1/airports:', () => {
+    it('GET /api/v1/airports - Should return status 200 and an Object', (done) => {
+      chai.request(app).get('/api/v1/api/v1/airports').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        done();
+      });
+    });
+    
+    it('GET /api/v1/airports?limit=3 - Should return status 200 and an Object with 3 airports', (done) => {
+      chai.request(app).get('/api/v1/api/v1/airports').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        console.log('res.body: %s',  Object.keys(res.body).length)
+        done();
+      });
+    });
+  })
 });
