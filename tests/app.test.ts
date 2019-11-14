07045545aac8b57bbe62c5b7240e56378c70ead0
Jens Kristian Hoel <github@hoelweb.com>
2019-11-14 20:42:36 +0100

    chg: added npm ci to build script

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -35,21 +35,21 @@ describe('Endpoint: /api/v1/:', function () {
 });
 
 
-// describe('Endpoint: /api/v1/airports:', () => {
-//   it('GET /api/v1/airports - Should return status 200 and all airports as an object', (done) => {
-//     chai.request(app).get('/api/v1/api/v1/airports').end((err, res) => {
-//       res.should.have.status(200);
-//       res.body.should.be.a('object');
-//       done();
-//     });
-//   });
+describe('Endpoint: /api/v1/airports:', () => {
+  it('GET /api/v1/airports - Should return status 200 and all airports as an object', function (done) {
+    chai.request(app).get('/api/v1/api/v1/airports').end(function (err, res) {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      done();
+    });
+  });
 
-//   it('GET /api/v1/airports?limit=3 - Should return status 200 and an object with 3 airports', (done) => {
-//     chai.request(app).get('/api/v1/api/v1/airports').end((err, res) => {
-//       res.should.have.status(200);
-//       res.body.should.be.a('object');
-//       console.log('res.body: %s', Object.keys(res.body).length)
-//       done();
-//     });
-//   });
-// })
+  it('GET /api/v1/airports?limit=3 - Should return status 200 and an object with 3 airports', function (done) {
+    chai.request(app).get('/api/v1/api/v1/airports').end(function (err, res) {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      console.log('res.body: %s', Object.keys(res.body).length)
+      done();
+    });
+  });
+})
