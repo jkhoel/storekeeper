5ef07a39dbab7afec1b7c17aecdb9e62e0afec48
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-14 11:24:45 +0100

    add: /api/v1/echo endpoint echoes back header and body of requests

--- src/routes/default.ts
+++ src/routes/default.ts
@@ -6,4 +6,11 @@ endpoint.get('/', (req, res) => {
   res.send({ msg: 'Storekeeper API is running!' });
 });
 
+endpoint.get('/echo', (req, res) => {
+  console.log('\n\nHeader: ', req.headers)
+  console.log('Body: ', req.body)
+
+  res.send({ header: req.headers, body: req.body || {} });
+});
+
 export default endpoint;
