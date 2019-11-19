8749da932c12be759531f75f82e1e0f6825de6b2
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-19 10:08:08 +0100

    chg: Operators use standard namings for the storekeeper database

--- src/operators/Airports.ts
+++ src/operators/Airports.ts
@@ -8,12 +8,13 @@ class Airports implements ISqlDataProvider {
 
   onGet(request: HTMLRequest, onQueryResult: HandlerCallback) {
     // Default: Get all records
-    let query = `SELECT * from list_airfields ORDER BY af_icao DESC`;
+    let query = `SELECT * from airfields ORDER BY icao DESC`;
 
     // Modify the query if we received any query parameters from the HTML request
     if (request.query) {
       // Get amount of records equal to the supplied limit
-      if (request.query.limit) query = query = `SELECT * from list_airfields ORDER BY af_icao DESC LIMIT 0, ${request.query.limit}`;
+      // TODO: This should have a property for DESC or ASC sorting
+      if (request.query.limit) query = query = `SELECT * from airfields ORDER BY icao DESC LIMIT 0, ${request.query.limit}`;
     }
 
     this.makeRequest(query, onQueryResult)
