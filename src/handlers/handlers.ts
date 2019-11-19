99a71c24ec2e55380986f0b4e1d79393e7946a02
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-19 13:31:39 +0100

    chg: fixed incorrect reference to 'Models'

--- src/handlers/handlers.ts
+++ src/handlers/handlers.ts
@@ -1,8 +1,8 @@
 type RouteCallback = (error?: Object, result?: Object) => void
 type QueryResult = { error?: Object, rows?: Object[] }
 
-const getHandler = (Model: any) => (request: Object, callback: RouteCallback) => {
-  Model.onGet(request, (result: QueryResult) => {
+const getHandler = (Operator: any) => (request: Object, callback: RouteCallback) => {
+  Operator.onGet(request, (result: QueryResult) => {
     if (result.error) return callback(result.error)
     callback(null, { status: 200, responseJson: { rows: result.rows } })
   })
