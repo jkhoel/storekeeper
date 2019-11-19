4c7ebfe4ecceb757f46c2088330de3b097e1c4ea
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-11-19 09:06:04 +0100

    chg: added todo's for future db checking on execution

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,12 @@ const ROOT_URL = process.env.ROOT_URL; // keys.rootUrl
 import endpoint from './routes/default';
 import airports from './routes/airports';
 
+// TODO: Do database creation and/or checks here if flags are passed to nodejs
+  // TODO: Check that .env holds required parameters
+  // TODO: Check that user is able to access the database
+  // TODO: Create database 'storekeeper' if it does not exist http://www.mysqltutorial.org/mysql-create-database/
+  // TODO: Create tables for all model files
+
 // Create a new express application instance
 const app: express.Application = express();
 
